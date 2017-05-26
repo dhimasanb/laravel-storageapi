66ecd6e5a87e82e471a09945a94264805aae1881
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-26 22:01:53 +0700

    Create index & add method

--- app/Http/Controllers/FileEntryController.php
+++ app/Http/Controllers/FileEntryController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fileentry;
+use Request;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Response;
 
 class FileEntryController extends Controller
 {
-    //
+    public function index()
+	{
+		$entries = Fileentry::all();
+ 
+		return view('fileentries.index', compact('entries'));
+	}
+ 
+	public function add() {
+ 
+		$file = Request::file('filefield');
+		$extension = $file->getClientOriginalExtension();
+		Storage::disk('local')->put($file->getFilename().'.'.$extension,  File::get($file));
+		$entry = new Fileentry();
+		$entry->mime = $file->getClientMimeType();
+		$entry->original_filename = $file->getClientOriginalName();
+		$entry->filename = $file->getFilename().'.'.$extension;
+ 
+		$entry->save();
+ 
+		return redirect('fileentry');
+		
+	}
 }
